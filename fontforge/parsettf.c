d8ad7d779dd8543369561901e7d28941ed8de6f9
Skef Iterum <github@skef.org>
2021-01-21 01:59:32 -0800

    Add comment about glyph names and encodings

--- fontforge/parsettf.c
+++ fontforge/parsettf.c
@@ -4558,6 +4558,9 @@ static int PickCMap(struct cmap_encs *cmap_encs,int enccnt,int def) {
 return( ret );
 }
 
+/* This doesn't worry about putting the most appropriate encoding
+   in sc->unicodeenc because (in theory) NameConsistancyCheck()
+   will swap it in. */
 static void addttfencoding(SplineChar *sc, int unc) {
     struct altuni *alt;
     if ( unc==-1 )
